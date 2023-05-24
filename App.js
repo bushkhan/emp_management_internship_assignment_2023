fdda5df2e8690118cd9350984d1a99e79f7822ca
Bushra Khan <bushrakhan2212002@gmail.com>
2023-05-24 22:40:03 +0530

    files added

--- App.js
+++ App.js
@@ -3,10 +3,34 @@ const app = express();
 const port = 3000;
 //configuration
 
+
+app.set("view engine", "hbs");
+app.set("views","./view")
+
 //Routing
 app.get("/",(req, res) => {
-    res.send("hello");
+    res.render('index')
 });
+app.get("/add",(req, res) => {
+    res.render('add')
+});
+
+app.get("/search",(req, res) => {
+    res.render('search')
+});
+
+app.get("/update",(req, res) => {
+    res.render('update')
+});
+
+app.get("/delete",(req, res) => {
+    res.render('delete')
+});
+
+app.get("/view",(req, res) => {
+    res.render('view')
+});
+
 
 //Server
 app.listen(port,(err)=>{
